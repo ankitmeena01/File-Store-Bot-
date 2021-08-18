e3c3e3e8f39a78af409c54e023e9a0522e312e7d
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2021-08-18 07:19:30 +0530

    Update save_media.py

--- handlers/save_media.py
+++ handlers/save_media.py
@@ -49,7 +49,7 @@ async def SaveBatchMediaInChannel(bot: Client, editable: Message, message_ids: l
         share_link = f"https://t.me/{Config.BOT_USERNAME}?start=JAsuran_{str_to_b64(str(SaveMessage.message_id))}"
         await editable.edit(
             f"**Video Link:\n\n{share_link}**\n\n"
-            f"**Just Click the link to get your Videos**",
+            f"**Just Click the link to get your Videos****",
             parse_mode="Markdown",
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("Open Link", url=share_link)],
@@ -89,7 +89,7 @@ async def SaveMediaInChannel(bot: Client, editable: Message, message: Message):
         share_link = f"https://t.me/{Config.BOT_USERNAME}?start=JAsuran_{str_to_b64(file_er_id)}"
         await editable.edit(
             f"**Video Link:\n\n{share_link}**\n\n"
-            f"**Just Click the link to get your Videos**",
+            f"**Just Click the link to get your Videos****",
             parse_mode="Markdown",
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("Open Link", url=share_link)],
